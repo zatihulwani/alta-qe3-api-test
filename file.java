9e1228d5b7d2a2b4a93b32a027a9549a693357c5
zatihulwani <zatihulwaani@gmail.com>
2022-04-18 20:29:56 +0700

    first commit

---
+++ file.java
@@ -0,0 +1,13 @@
+public class file {
+
+    @ParameterType(".*")
+    public Actor actor(String actorName) {
+        return OnStage.theActorCalled(actorName);
+    }
+
+    @Before
+    public void setTheStage() {
+        OnStage.setTheStage(new OnlineCast());
+    }
+}
+}
